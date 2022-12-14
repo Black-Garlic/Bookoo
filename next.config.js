bc8e1e7a1ccd13219c62e5e6022594fd0ba8f226
kyungyup <rudduqdl@naver.com>
2022-12-14 10:17:29 +0900

    feature: 환경 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
+const {parsed: dotenv} = require('dotenv').config({path: `env/.${isDev? 'dev' : 'prod'}.env`})
+console.log('[ENV]', dotenv)
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {...dotenv},
   async redirects() {
     return [
       {
